aefffd774a33c7772614199c314295dc546e1ee8
Eric Moran <emoran0403@gmail.com>
2022-10-25 10:48:45 -0400

    added types file, added dogfacts component skeleton

---
+++ Types.ts
@@ -0,0 +1,1 @@
+export interface NO_PROPS {}
--- src/client/DogFacts.tsx
+++ src/client/DogFacts.tsx
@@ -1,15 +1,14 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import * as Types from "../../Types";
-import DogFacts from "./DogFacts";
 
-const App = (props: Types.NO_PROPS) => {
+const DogFacts = (props: Types.NO_PROPS) => {
   return (
     <main className="container my-5">
-      <h1 className="text-primary text-center">Hello!</h1>
-      <DogFacts />
+      <h1 className="text-primary text-center">This is Dog Facts</h1>
+      <div>Some dog facts will go here</div>
     </main>
   );
 };
 
-export default App;
+export default DogFacts;
